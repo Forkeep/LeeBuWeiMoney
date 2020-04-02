013bab6591fbf2c8f76f6b1e86006c862f16eea7
Forkeep <13126593533@163.com>
2020-04-02 12:55:39 +0800

    【引入Icon】svg-sprite-loader & .d.ts & vue.config.js

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="nav">
-    <router-link to="/money">money</router-link>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#money"/>
+      </svg>
+      money
+    </router-link>
     |
-    <router-link to="/labels">labels</router-link>
+    <router-link to="/labels">
+      <svg>
+        <use xlink:href="#labels"/>
+      </svg>
+      labels
+    </router-link>
     |
-    <router-link to="/statistics">statistics</router-link>
+    <router-link to="/statistics">
+      <svg>
+        <use xlink:href="#statistics"/>
+      </svg>
+      statistics
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
+  import labels from '@/assets/icons/labels.svg';
+  import money from '@/assets/icons/money.svg';
+  import statistics from '@/assets/icons/statistics.svg';
+
+  console.log(labels);
+  console.log(money);
+  console.log(statistics);
   import {Component} from 'vue-property-decorator';
+
 
   @Component
   export default class Nav extends Vue {
