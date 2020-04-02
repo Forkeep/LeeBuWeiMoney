348947231318e977bea002c624b93c38180f0689
Forkeep <13126593533@163.com>
2020-04-02 11:40:43 +0800

    插入导航栏<Nav/>

---
+++ src/components/Nav.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <router-link to="/money">money</router-link>|
+    <router-link to="/labels">labels</router-link>|
+    <router-link to="/statistics">statistics</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Nav extends Vue {
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
