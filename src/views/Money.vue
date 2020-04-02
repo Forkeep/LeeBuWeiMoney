6768d14a25f00d03e26e40bb8c2dda6c6578d8f3
Forkeep <13126593533@163.com>
2020-04-02 18:40:16 +0800

    【Money样式】标签栏样式调整可滚动屏幕

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -28,12 +28,35 @@
     <div class="new">
       <button>新增标签</button>
     </div>
-    <ul class="tags">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-    </ul>
+    <div class="tags">
+      <div class="placeholder"></div>
+      <ul>
+        <li>衣</li>
+        <li>食</li>
+        <li>衣</li>
+        <li>衣</li>
+        <li>食</li>
+        <li>衣</li>
+        <li>衣</li>
+        <li>食</li>
+        <li>衣</li>
+        <li>衣</li>
+        <li>食</li>
+        <li>衣</li>
+        <li>食</li>
+        <li>衣</li>
+        <li>食</li>
+        <li>食</li>
+        <li>衣</li>
+        <li>食</li>
+        <li>食</li>
+        <li>衣</li>
+        <li>食</li>
+        <li>食</li>
+        <li>衣</li>
+        <li>食</li>
+      </ul>
+    </div>
   </LayoutNav>
 </template>
 
@@ -62,14 +85,28 @@
 
   .layout-nav {
     .tags {
+      flex-grow: 1;
+      overflow: auto;
       display: flex;
-      align-items: center;
+      flex-direction: column;
+      .placeholder{
+        flex-grow: 1;
+      }
+      ul {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
 
-      li {
-        margin: 0 5px 0 5px;
-        background-color: #ddd;
-        width: 50px;
-        border-radius: 10px;
+        li {
+          margin: 3px 5px 3px 5px;
+          background-color: #ddd;
+          width: 50px;
+          max-height: 24px;
+          border-radius: 10px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
       }
     }
 
